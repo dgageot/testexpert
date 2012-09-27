a96784c9931e427a1e409360a38e4e48a1940eef
David Gageot <david@gageot.net>
2012-09-27 18:54:20 +0200

    Simplify ServiceRule

--- src/main/java/net/gageot/test/rules/ServiceRule.java
+++ src/main/java/net/gageot/test/rules/ServiceRule.java
@@ -19,67 +19,76 @@
  */
 package net.gageot.test.rules;
 
-import com.google.common.util.concurrent.*;
-import com.google.inject.*;
-import org.junit.rules.*;
+import com.google.inject.Module;
+import org.junit.rules.ExternalResource;
 
-import java.util.*;
+import java.util.Random;
 
 /**
  * JUnit Rule to start/stop a service before/after tests.
  */
-public class ServiceRule<T extends Service> extends ExternalResource {
-	private static final int TRY_COUNT = 10;
-	private static final int DEFAULT_PORT = 8183;
+public class ServiceRule<T> extends ExternalResource {
+  private static final int TRY_COUNT = 10;
+  private static final int DEFAULT_PORT = 8183;
 
-	private final Class<T> serviceClass;
-	private final Module[] modules;
-	private final Random random = new Random();
-	private T service;
+  private final Class<T> serviceClass;
+  private final Module[] modules;
+  private final Random random = new Random();
+  private T service;
+  private int port;
 
-	private ServiceRule(Class<T> serviceClass, Module... modules) {
-		this.serviceClass = serviceClass;
-		this.modules = modules;
-	}
+  private ServiceRule(Class<T> serviceClass, Module... modules) {
+    this.serviceClass = serviceClass;
+    this.modules = modules;
+  }
 
-	public static <T extends Service> ServiceRule<T> startWithRandomPort(Class<T> serviceClass, Module... modules) {
-		return new ServiceRule<T>(serviceClass, modules);
-	}
+  public static <T> ServiceRule<T> startWithRandomPort(Class<T> serviceClass, Module... modules) {
+    return new ServiceRule<T>(serviceClass, modules);
+  }
 
-	@Override
-	protected void before() {
-		for (int i = 0; i < TRY_COUNT; i++) {
-			try {
-				service = createServive(randomPort());
-				service.startAndWait();
-				return;
-			} catch (Exception e) {
-				System.err.println("Unable to bind server: " + e);
-			}
-		}
-		throw new IllegalStateException("Unable to start server");
-	}
+  @Override
+  protected void before() {
+    for (int i = 0; i < TRY_COUNT; i++) {
+      try {
+        port = randomPort();
+        service = createServive();
+        service.getClass().getMethod("start", int.class).invoke(service, port);
+        return;
+      } catch (Exception e) {
+        System.err.println("Unable to bind server: " + e);
+      }
+    }
+    throw new IllegalStateException("Unable to start server");
+  }
 
-	@Override
-	protected void after() {
-		if (service != null) {
-			service.stopAndWait();
-		}
-	}
+  @Override
+  protected void after() {
+    if (service != null) {
+      try {
+        service.getClass().getMethod("stop").invoke(service);
+      } catch (Exception e) {
+        System.err.println("Unable to stop server: " + e);
+      }
+    }
+  }
 
-	private T createServive(int port) throws Exception {
-		try {
-			return serviceClass.getDeclaredConstructor(int.class, Module[].class).newInstance(port, modules);
-		} catch (Exception e) {
-			return serviceClass.getDeclaredConstructor(int.class).newInstance(port);
-		}
-	}
+  private T createServive() throws Exception {
+    try {
+      return serviceClass.getDeclaredConstructor(Module[].class).newInstance(modules);
+    } catch (Exception e) {
+      return serviceClass.getDeclaredConstructor().newInstance();
+    }
+  }
 
-	public T service() {
-		return service;
-	}
+  public T service() {
+    return service;
+  }
 
-	private synchronized int randomPort() {
-		return DEFAULT_PORT + random.nextInt(1000);
-	}
+  public int getPort() {
+    return port;
+  }
+
+  private synchronized int randomPort() {
+    return DEFAULT_PORT + random.nextInt(1000);
+  }
 }
